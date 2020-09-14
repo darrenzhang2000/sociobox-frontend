db6384ae072750ea1fa8d9c408dbab843553f281
Darren Zhang <darrenzhang@Darrens-MacBook-Pro.local>
2020-09-14 13:39:46 -0400

    added header and footer

--- src/App.js
+++ src/App.js
@@ -5,17 +5,18 @@ import Register from './pages/register/register.page';
 import Landing from './pages/landing/landing.page';
 import { Provider } from 'react-redux';
 import store from './redux/redux';
-
+import Header from './component/header/header.component'
+import Bottom from './component/bottom/bottom.component'
 
 function App() {
   return (
     <Provider store={store}>
 
     <div>
-      {/* <Header /> */}
+      <Header />
       <Route exact path='/' component={Landing} />
       <Route exact path='/register' component={Register} />
-      {/* <Bottom /> */}
+      <Bottom />
     </div>
     </Provider>
 
