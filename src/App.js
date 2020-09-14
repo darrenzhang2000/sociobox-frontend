768f51cce91536a5d87469e6be29239c84a5d45f
Darren Zhang <darrenzhang@Darrens-MacBook-Pro.local>
2020-09-14 09:29:41 -0400

    added landing template

--- src/App.js
+++ src/App.js
@@ -1,25 +1,24 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import { Route } from 'react-router-dom';
+import Register from './pages/register/register.page';
+import Landing from './pages/landing/landing.page';
+import { Provider } from 'react-redux';
+import store from './redux/redux';
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <Provider store={store}>
+
+    <div>
+      {/* <Header /> */}
+      <Route exact path='/' component={Landing} />
+      <Route exact path='/register' component={Register} />
+      {/* <Bottom /> */}
     </div>
+    </Provider>
+
   );
 }
 
