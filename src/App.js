0f63a402d0187269996a429843b4c6e787c1c7af
Darren Zhang <darrenzhang@Darrens-MacBook-Pro.local>
2020-09-14 21:10:18 -0400

    added article component

--- src/App.js
+++ src/App.js
@@ -7,40 +7,54 @@ import { Provider } from 'react-redux';
 import store from './redux/redux';
 import Logo from './logo.svg';
 import 'materialize-css/dist/css/materialize.min.css';
+import Podcast from './component/podcasts/podcast';
+import Article from './component/articles/article';
 
 function App() {
   return (
     <Provider store={store}>
-    <div className="navbar-fixed navbar-top">
-      <nav className="white">
-       <div class="nav-wrapper">
-          <a href="#" class="brand-logo left"><img src={Logo}></img></a>
-          <ul class="right">
-            <li><a class="waves-effect waves-light btn btn-base btn-small-radius">Login/ Register</a></li>
-          </ul>
-        </div>
-      </nav>
-    </div>
+      hi
 
-    <div>
-      {/* <Header /> */}
-      {/* <Route exact path='/' component={Landing} />
+      {/* <iframe
+    allow="microphone;"
+    width="350"
+    height="430"
+    src="https://console.dialogflow.com/api-client/demo/embedded/936f0a27-ec68-453f-941f-61b8d32b154e">
+</iframe> */}
+
+      <div className="navbar-fixed navbar-top">
+        <nav className="white">
+          <div class="nav-wrapper">
+            <a href="#" class="brand-logo left"><img src={Logo}></img></a>
+            <ul class="right">
+              <li><a class="waves-effect waves-light btn btn-base btn-small-radius">Login/ Register</a></li>
+            </ul>
+          </div>
+        </nav>
+      </div>
+
+      <div>
+
+        {/* <Header /> */}
+        {/* <Route exact path='/' component={Landing} />
       <Route exact path='/register' component={Register} /> */}
-      {/* <Bottom /> */}
-    </div>
-    <div className="navbar-fixed navbar-bottom">
-      <nav className="black">
-       <div class="nav-wrapper">
-          <ul class="center">
-            <li><a href="sass.html">Site map</a></li>
-            <li><a href="badges.html">Terms</a></li>
-            <li><a href="badges.html">Privacy</a></li>
-            <li><a href="badges.html">Contact Us</a></li>
-          </ul>
-        </div>
-      </nav>
-    </div>
-    
+        {/* <Bottom /> */}
+        <Route exact path="/podcast" component={Podcast} />
+        <Route exact path="/article" component={Article} />
+      </div>
+      <div className="navbar-fixed navbar-bottom">
+        <nav className="black">
+          <div class="nav-wrapper">
+            <ul class="center">
+              <li><a href="sass.html">Site map</a></li>
+              <li><a href="badges.html">Terms</a></li>
+              <li><a href="badges.html">Privacy</a></li>
+              <li><a href="badges.html">Contact Us</a></li>
+            </ul>
+          </div>
+        </nav>
+      </div>
+
     </Provider>
 
   );
