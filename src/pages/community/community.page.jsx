2bbc8f601e327f1c3684628eb34cd947b1acdd5f
matmany <matmanymat@gmail.com>
2020-09-15 01:51:25 -0300

    post and forum with basic visual

--- src/pages/community/community.page.jsx
+++ src/pages/community/community.page.jsx
@@ -39,7 +39,7 @@ function Forum(){
                 
                 <h5>Please note that any response seen as disrespectful will be removed.</h5>
             </div>
-            <div className="col s4">
+            <div id="option" className="col s4">
                 <div className="input-field col s11 ">
                         <input
                         type="text"
@@ -50,13 +50,13 @@ function Forum(){
                 <div className="col s1">
                     <i className="material-icons searchIcon">search</i>
                   </div>
-                <a href="">+New Post</a>
+                <a className="waves-effect waves-light btn btn-base btn-small-radius">+New Post</a>
             </div>
             <div id="discussion" className="col s12">
                 <h6 id="title">General Discussions</h6>
                 <ul claclassNamess="collection">
-                    <li class="collection-item avatar">
-                        <div className="disc-item">
+                    <li className="collection-item avatar">
+                       <Link to="/community/post"> <div className="disc-item">
                             <div>
                             <img src={UserSvg} alt="" class="circle"/>
                             <p>Should we be more concerned about plastic’s impact on the environment or our own health?</p>
@@ -65,8 +65,9 @@ function Forum(){
                             {/* <a href="#!" class="secondary-content"><i class="material-icons">grade</i></a> */}
                         </div>
                         <a>32 comments</a>
+                        </Link>
                     </li>
-                    <li class="collection-item avatar">
+                    <li className="collection-item avatar">
                          <div className="disc-item">
                             <div>
                             <img src={UserSvg} alt="" class="circle"/>
@@ -85,8 +86,65 @@ function Forum(){
 
 function Post(){
     return(
-        <div>
-            <h1>Hey Post here</h1>
+        <div className="row post">
+            <div className="col s12 post-data">
+                <div id="post-head">
+                    <img src={UserSvg} alt="" class="circle"/>
+                    <h2>Should we be more concerned about plastic’s impact on the environment or our own health? </h2>
+                </div>
+                <p>
+                    Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sit auctor pretium donec convallis. 
+                    Duis vel non in in tincidunt. Et senectus aliquam eget nulla nunc nulla porta vel ac. 
+                    Ornare id velit, ut semper ut arcu. Mauris, lectus amet, mi quis vitae nisi. Adipiscing risus mollis dui posuere. 
+                    In non quisque faucibus vel dictum accumsan in. In vel, cursus odio tellus semper dui. Enim nibh orci cursus id tellus pharetra erat eleifend integer. 
+                    Habitant hac massa morbi risus urna eget nulla cras. Et cras consequat magna mauris, malesuada pellentesque.
+                </p>
+                <div className="right-align">
+                    <a className="waves-effect waves-light btn btn-base btn-radius">+New Post</a>
+                </div>
+            </div>
+            <div className="col s12 input-comment">
+                <div id="user-row">
+                    <div id="user-reply">
+                        <img src={UserSvg} alt="" class="circle"/>
+                        <textarea id="textarea1" class="materialize-textarea"></textarea>
+                    </div>
+                    <a className="waves-effect waves-light btn btn-base btn-radius">Submit</a>
+
+                </div>
+            </div>
+            <div className="col s12 comments">
+                <ul claclassNamess="collection">
+                        <li className="collection-item avatar">
+                        <Link to="/community/post"> <div className="disc-item">
+                                <div>
+                                <img src={UserSvg} alt="" class="circle"/>
+                                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. 
+                                    Ut id viverra ipsum, lectus ante faucibus lacus est. 
+                                    Adipiscing sit at sit viverra netus mauris vel. 
+                                    Molestie ac tortor mi velit eget semper.</p>
+                                </div>
+                                <label>30 min ago</label>
+                                {/* <a href="#!" class="secondary-content"><i class="material-icons">grade</i></a> */}
+                            </div>
+                            <a>32 comments</a>
+                            </Link>
+                        </li>
+                        <li className="collection-item avatar">
+                            <div className="disc-item">
+                                <div>
+                                <img src={UserSvg} alt="" class="circle"/>
+                                <p>Dolor sit amet, consectetur adipiscing elit. 
+                                    Lectus mi, pellentesque ut dolor. 
+                                    Odio egestas malesuada semper felis.</p>
+                                </div>
+                                <label>2 hours ago</label>
+                                {/* <a href="#!" class="secondary-content"><i class="material-icons">grade</i></a> */}
+                            </div>
+                            <a>32 comments</a>
+                        </li>
+                    </ul> 
+            </div>
         </div>
     )
 }
